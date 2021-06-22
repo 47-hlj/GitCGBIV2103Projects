eada7d5ed9e5cae09280a7e0462b62fd322ea789
47-hlj <1725816370@qq.com>
2021-06-22 15:42:48 +0800

    第四阶段——微服务架构

---
+++ 02-jt/jt-common/jt-common-basic/src/main/java/com/cy/jt/common/basic/util/StringUtils.java
@@ -0,0 +1,7 @@
+package com.cy.jt.common.basic.util;
+
+public class StringUtils {
+    public static boolean isEmpty(String str) {
+        return str == null || "".equals(str);
+    }
+}
--- 02-jt/jt-portal/jt-member/src/main/java/com/cy/jt/MemberApplication.java
+++ 02-jt/jt-portal/jt-member/src/main/java/com/cy/jt/MemberApplication.java
@@ -1,6 +1,7 @@
 package com.cy.jt;
 
 import org.springframework.boot.SpringApplication;
+import com.cy.jt.common.basic.util.StringUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -10,5 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class,args);
+        System.out.println(StringUtils.isEmpty("hello"));
     }
 }
