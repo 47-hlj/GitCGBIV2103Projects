79eca2ad2fac27e1f0f196fbb6dff51819d5aaf9
47-hlj <1725816370@qq.com>
2021-06-22 15:22:06 +0800

    第四阶段——微服务架构

---
+++ 02-jt/jt-portal/jt-member/src/main/java/com/cy/jt/MemberApplication.java
@@ -0,0 +1,14 @@
+package com.cy.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 这是springboot启动类
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class,args);
+    }
+}
