de9493ba5e4ed44672c5599e501ecd4e11b1c1ee
47-hlj <1725816370@qq.com>
2021-06-23 11:52:00 +0800

    DemoController实现CRUD

--- 02-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/controller/DemoController.java
+++ 02-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/controller/DemoController.java
@@ -1,18 +1,43 @@
 package com.cy.jt.demo.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * 通过此对象演示CRUD请求的处理
+ * restful规范=动作+url
+ * @GetMapping
+ * @PostMapping
+ * @PutMapping
+ * @DeleteMapping
+ * @PatchMapping
+ */
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
+    @PutMapping
+    public String doUpdate(@RequestBody  Map<String,Object> map){
+        return map.toString()+" is updated";
+    }
+
+    @PostMapping
+    public String doCreate(@RequestBody  Map<String,Object> map){
+        return map.toString()+" is created";
+    }
+
+    @GetMapping("{id}")
+    public String doRetrieveById(@PathVariable Integer ...id){
+        return "find result by "+id;
+    }
+
+
 //    @RequestMapping
-    @DeleteMapping
-    public String doDeleteById(Integer ...id){
+    @DeleteMapping("{id}")
+    public String doDeleteById(@PathVariable Integer ...id){
         return Arrays.toString(id)+" id deleted";
     }
 }
