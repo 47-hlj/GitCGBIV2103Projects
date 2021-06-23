ddb413543becbdf306c5bd4defde1346a89d524b
47-hlj <1725816370@qq.com>
2021-06-23 15:27:42 +0800

    tomcat线程

--- 02-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/controller/DemoController.java
+++ 02-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/controller/DemoController.java
@@ -1,9 +1,12 @@
 package com.cy.jt.demo.controller;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 通过此对象演示CRUD请求的处理
@@ -15,9 +18,25 @@ import java.util.Map;
  * @PatchMapping
  */
 
+//@Lazy  延迟加载
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+
+    DemoController(){
+        System.out.println("DemoController()");
+    }
+
+    //J.U.C 包下的线程安全的对象(提供了对长整型数据的原子操作，底层CAS算法)
+    private AtomicLong counter=new AtomicLong(1);
+    @GetMapping("{id}")
+    public String doRetrieveById(@PathVariable Integer id) throws Exception{
+        String tName = Thread.currentThread().getName();
+        System.out.println(counter.getAndIncrement());
+//        Thread.sleep(3000);
+        TimeUnit.SECONDS.sleep(3);
+        return tName+ "find result by "+id;
+    }
 
     @PutMapping
     public String doUpdate(@RequestBody  Map<String,Object> map){
@@ -28,12 +47,6 @@ public class DemoController {
     public String doCreate(@RequestBody  Map<String,Object> map){
         return map.toString()+" is created";
     }
-
-    @GetMapping("{id}")
-    public String doRetrieveById(@PathVariable Integer ...id){
-        return "find result by "+id;
-    }
-
 
 //    @RequestMapping
     @DeleteMapping("{id}")
