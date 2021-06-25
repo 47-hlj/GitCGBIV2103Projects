a05fb3885e0a8814e12d9cee8f7b86396e3d01c5
47-hlj <1725816370@qq.com>
2021-06-25 14:19:53 +0800

    02-jt

---
+++ 02-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/service/MemoryService.java
@@ -0,0 +1,31 @@
+package com.cy.jt.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class MemoryService {
+
+    private Map<String,Object> cache=new ConcurrentHashMap<String, Object>();
+    public List<Map<String,Object>> list(){
+        if(cache.containsKey("memoryKey"))
+            return (List<Map<String, Object>>)cache.get("memoryKey");
+        System.out.println("get data from database");
+        Map<String,Object> m1=new HashMap<String, Object>();
+        Map<String,Object> m2=new HashMap<String, Object>();
+        m1.put("id",100);
+        m1.put("title","title-A");
+        m2.put("id",101);
+        m2.put("title","title-B");
+        List<Map<String , Object>> list = new ArrayList<Map<String, Object>>();
+        list.add(m1);
+        list.add(m2);
+        cache.put("memoryKey",list);
+        return list;
+    }
+}
