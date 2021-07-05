988157c3ea3879f1d109a7578436342c43c884d3
47-hlj <hlj172532>
2021-07-05 10:40:56 +0800

    Gateway

---
+++ 03-jt-sca/sca-gateway/src/main/java/com/cy/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 47HLJ
+ * @date 2021/7/5 10:33
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
