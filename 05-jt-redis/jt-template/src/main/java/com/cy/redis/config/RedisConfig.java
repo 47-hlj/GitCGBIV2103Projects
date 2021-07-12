e21d8de5cc23e3b61557be7b407041baff87a545
47-hlj <hlj172532>
2021-07-12 14:28:16 +0800

    redis

--- 05-jt-redis/jt-template/src/main/java/com/cy/redis/config/RedisConfig.java
+++ 05-jt-redis/jt-template/src/main/java/com/cy/redis/config/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
  * @author 47HLJ
  * @date 2021/7/12 13:47
@@ -22,18 +23,21 @@ public class RedisConfig {
         RedisTemplate<Object,Object> redisTemplate=new RedisTemplate<>();
         //2.设置连接工厂
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        //3.定义序列化方式
+        //3.定义序列化方式(在这里选择了jackson)
         Jackson2JsonRedisSerializer redisSerializer= new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper objectMapper=new ObjectMapper();
+        //设置要序列化的域(通过所以get方法获取所有属性名以及值)
         //所有访问修饰符修饰的方法,any表示任意级别访问修饰符修饰的属性
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        //启动输入域检查(类不能是final修饰的),并基于json串中的key将其值存储到pojo对象
+        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
         redisSerializer.setObjectMapper(objectMapper);
         //4.设置RedisTemplate的序列化
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(redisSerializer);
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(redisSerializer);
+        //spring规范中假如修改bean对象的默认特性,建议调用一下afterPropertiesSet()
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
