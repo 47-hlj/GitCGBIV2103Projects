c9f58814a7b9a58164943a23985f00cd263dcf2f
47-hlj <hlj172532>
2021-07-20 19:03:49 +0800

    Spring Security

--- 06-jt-security/jt-spring-security/src/main/java/com/cy/jt/controller/ResourceController.java
+++ 06-jt-security/jt-spring-security/src/main/java/com/cy/jt/controller/ResourceController.java
@@ -9,15 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ResourceController {
-    /*访问doCreate方法必须拥有/doCreate权限*/
+
+    /**
+     * 访问doCreate方法必须拥有/doCreate权限
+     * @return
+     */
     @PreAuthorize("hasAuthority('sys:res:create')")
     @RequestMapping("/doCreate")
     public String doCreate(){
         return "add resource";
     }
 
-    /*访问doUpdate方法时必须拥有jinpai角色*/
-    @PreAuthorize("hasRole('jinpai')") //RBAC
+    /**
+     * 访问doUpdate方法时必须拥有jinpai角色
+     * @return
+     */
+    @PreAuthorize("hasRole('jinpai')")
     @RequestMapping("doUpdate")
     public String doUpdate(){
         //......
